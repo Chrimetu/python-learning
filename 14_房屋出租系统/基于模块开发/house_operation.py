d36e2d882141fb8cd4dfb148e07a7bf8d2abcd5a
Chirmera_tu <95748451+Chrimetu@users.noreply.github.com>
2026-01-10 17:03:10 +0800

    Add files via upload

---
+++ 14_房屋出租系统/基于模块开发/house_operation.py
@@ -0,0 +1,13 @@
+def main_menu():
+    """
+    显示主菜单，供用户选择
+    """
+    print()
+    print("房屋出租系统菜单".center(32,"="))
+    print("\t\t\t1 新 增 房 源")
+    print("\t\t\t2 查 找 房 屋")
+    print("\t\t\t3 删 除 房 屋 信 息")
+    print("\t\t\t4 修 改 房 屋 列 表")
+    print("\t\t\t5 房 屋 列 表")
+    print("\t\t\t6 退 出")
+
