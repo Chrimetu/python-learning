9c1e13356f0b2b3aaec3780290125cefa9e93bb6
xuantu <2832551008@qq.com>
2026-09-05 17:56:31 +0800

    添加翻译后的吴恩达机器学习笔记和课程习题

---
+++ 吴恩达-课件与课后习题/第二课 Advanced Learning Algorithms/week2/5.Multiclass Classification/lab_utils_multiclass_TF.py
@@ -0,0 +1,191 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+import warnings
+from matplotlib import cm
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import matplotlib.colors as colors
+from lab_utils_common import dlc
+
+dlc = dict(dlblue='#0096ff', dlorange='#FF9300', dldarkred='#C00000',
+           dlmagenta='#FF40FF', dlpurple='#7030A0', dldarkblue='#0D5BDC')
+dlblue = '#0096ff'
+dlorange = '#FF9300'
+dldarkred = '#C00000'
+dlmagenta = '#FF40FF'
+dlpurple = '#7030A0'
+dldarkblue = '#0D5BDC'
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+
+plt.style.use('./deeplearning.mplstyle')
+
+dkcolors = plt.cm.Paired((1,3,7,9,5,11))
+ltcolors = plt.cm.Paired((0,2,6,8,4,10))
+dkcolors_map = mpl.colors.ListedColormap(dkcolors)
+ltcolors_map = mpl.colors.ListedColormap(ltcolors)
+
+def plot_cat_decision_boundary_mc(ax, X, predict, class_labels=None, legend=False, vector=True):
+    x_min, x_max = X[:, 0].min()-0.5, X[:, 0].max()+0.5
+    y_min, y_max = X[:, 1].min()-0.5, X[:, 1].max()+0.5
+    h = max(x_max-x_min, y_max-y_min)/100
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    points = np.c_[xx.ravel(), yy.ravel()]
+
+    if vector:
+        Z = predict(points)
+    else:
+        Z = np.zeros((len(points),))
+        for i in range(len(points)):
+            Z[i] = predict(points[i].reshape(1,2))
+
+    Z = Z.reshape(xx.shape)
+    ax.contour(xx, yy, Z, linewidths=1)
+
+def plot_mc_data(X, y, class_labels=None, legend=False, size=40):
+    classes = np.unique(y)
+    for i in classes:
+        label = class_labels[i] if class_labels else "class {}".format(i)
+        idx = np.where(y == i)
+        plt.scatter(X[idx, 0], X[idx, 1],
+                    edgecolor='black', s=size, label=label)
+    if legend:
+        plt.legend()
+
+def plt_mc_data(ax, X, y, classes, class_labels=None, map=plt.cm.Paired,
+                legend=False, size=50, m='o', equal_xy=False):
+    for i in range(classes):
+        idx = np.where(y == i)
+        col = len(idx[0]) * [i]
+        label = class_labels[i] if class_labels else "c{}".format(i)
+
+        ax.scatter(X[idx, 0], X[idx, 1],
+                   marker=m,
+                   c=col,
+                   vmin=0,
+                   vmax=map.N,
+                   cmap=map,
+                   s=size,
+                   label=label)
+
+    if legend:
+        ax.legend()
+    if equal_xy:
+        ax.axis("equal")
+
+def plt_mc(X_train, y_train, classes, centers, std):
+    fig, ax = plt.subplots(1,1,figsize=(3,3))
+    for attr in ("toolbar_visible", "header_visible", "footer_visible"):
+        if hasattr(fig.canvas, attr):
+            setattr(fig.canvas, attr, False)
+
+    plt_mc_data(ax, X_train, y_train, classes,
+                map=dkcolors_map, legend=True, size=50, equal_xy=False)
+    ax.set_title("Multiclass Data")
+    ax.set_xlabel("x0")
+    ax.set_ylabel("x1")
+    plt.show()
+
+def plt_cat_mc(X_train, y_train, model, classes):
+    model_predict = lambda Xl: np.argmax(model.predict(Xl), axis=1)
+
+    fig, ax = plt.subplots(1,1, figsize=(3,3))
+    for attr in ("toolbar_visible", "header_visible", "footer_visible"):
+        if hasattr(fig.canvas, attr):
+            setattr(fig.canvas, attr, False)
+
+    plt_mc_data(ax, X_train, y_train, classes,
+                map=dkcolors_map, legend=True)
+    plot_cat_decision_boundary_mc(ax, X_train, model_predict, vector=True)
+    ax.set_title("model decision boundary")
+    plt.xlabel(r'$x_0$')
+    plt.ylabel(r'$x_1$')
+    plt.show()
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    return colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(
+            n=cmap.name, a=minval, b=maxval
+        ),
+        cmap(np.linspace(minval, maxval, n))
+    )
+
+def plt_prob_z(ax, fwb, x0_rng=(-8,8), x1_rng=(-5,4)):
+    x0_space = np.linspace(x0_rng[0], x0_rng[1], 40)
+    x1_space = np.linspace(x1_rng[0], x1_rng[1], 40)
+
+    tmp_x0, tmp_x1 = np.meshgrid(x0_space, x1_space)
+    z = np.zeros_like(tmp_x0)
+    c = np.zeros_like(tmp_x0)
+
+    for i in range(tmp_x0.shape[0]):
+        for j in range(tmp_x1.shape[1]):
+            x = np.array([[tmp_x0[i,j], tmp_x1[i,j]]])
+            z[i,j] = fwb(x)
+            c[i,j] = 0. if z[i,j] == 0 else 1.
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        ax.contour(tmp_x0, tmp_x1, c, linewidths=1)
+
+    cmap = plt.get_cmap('Blues')
+    new_cmap = truncate_colormap(cmap, 0.0, 0.7)
+
+    pcm = ax.pcolormesh(
+        tmp_x0, tmp_x1, z,
+        norm=cm.colors.Normalize(vmin=np.amin(z), vmax=np.amax(z)),
+        cmap=new_cmap, shading='nearest', alpha=0.9
+    )
+    ax.figure.colorbar(pcm, ax=ax)
+
+def plt_layer_relu(X, Y, W1, b1, classes):
+    nunits = W1.shape[1]
+    Y = Y.reshape(-1,)
+
+    fig, ax = plt.subplots(1, nunits, figsize=(7,2.5))
+    for attr in ("toolbar_visible", "header_visible", "footer_visible"):
+        if hasattr(fig.canvas, attr):
+            setattr(fig.canvas, attr, False)
+
+    ax = np.atleast_1d(ax)
+
+    for i in range(nunits):
+        layerf = lambda x, i=i: np.maximum(0, np.dot(x, W1[:,i]) + b1[i])
+        plt_prob_z(ax[i], layerf)
+        plt_mc_data(ax[i], X, Y, classes,
+                    map=dkcolors_map, legend=True, size=50, m='o')
+        ax[i].set_title(f"Layer 1 Unit {i}")
+        ax[i].set_ylabel(r"$x_1$", size=10)
+        ax[i].set_xlabel(r"$x_0$", size=10)
+
+    fig.tight_layout()
+    plt.show()
+
+def plt_output_layer_linear(X, Y, W, b, classes, x0_rng=None, x1_rng=None):
+    nunits = W.shape[1]
+    Y = Y.reshape(-1,)
+
+    fig, ax = plt.subplots(2, int(nunits/2), figsize=(7,5))
+    for attr in ("toolbar_visible", "header_visible", "footer_visible"):
+        if hasattr(fig.canvas, attr):
+            setattr(fig.canvas, attr, False)
+
+    for i, axi in enumerate(ax.flat):
+        layerf = lambda x, i=i: np.dot(x, W[:,i]) + b[i]
+        plt_prob_z(axi, layerf, x0_rng=x0_rng, x1_rng=x1_rng)
+        plt_mc_data(axi, X, Y, classes,
+                    map=dkcolors_map, legend=True, size=50, m='o')
+
+        axi.set_ylabel(r"$a^{[1]}_1$", size=9)
+        axi.set_xlabel(r"$a^{[1]}_0$", size=9)
+
+        if x0_rng is not None:
+            axi.set_xlim(x0_rng)
+        if x1_rng is not None:
+            axi.set_ylim(x1_rng)
+
+        axi.set_title(f"Linear Output Unit {i}")
+
+    fig.tight_layout()
+    plt.show()
